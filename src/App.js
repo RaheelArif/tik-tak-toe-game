72abd10a82fe57a06708f16fe4a6cb582e563873
RaheelArif <phuawei292@gmail.com>
2019-02-14 13:06:27 +0500

    check for  winner and replay button added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ class App extends Component {
   state = {
     text: Array(9).fill(null),
     player: null,
+    winner: null
+  }
 
+  checkWinner() {
+    let winnerStatus = [
+      ["0", "1", "2"],
+      ["3", "4", "5"],
+      ["6", "7", "8"],
+      ["0", "3", "6"],
+      ["1", "4", "7"],
+      ["2", "5", "8"],
+      ["0", "4", "8"],
+      ["2", "4", "6"],
+    ]
+    for (let index = 0; index < winnerStatus.length; index++) {
+      const [a, b, c] = winnerStatus[index];
+      if (this.state.text[a] && this.state.text[a] === this.state.text[b] && this.state.text[a] === this.state.text[c]) {
+        alert("winner is " + this.state.player)
+
+        this.setState({
+          winner: this.state.player
+        })
+      }
+    }
   }
   handleClick = (index) => {
-    if (this.state.player !== null) {
+    if (this.state.player !== null && this.state.winner === null) {
       let newText = this.state.text
       if (newText[index] === null) {
         newText[index] = this.state.player
@@ -17,14 +40,21 @@ class App extends Component {
           text: newText,
           player: newPlayer
         })
-
+        this.checkWinner()
       }
     }
 
   }
-  SelectPlayer(selectedPlayer){
+  SelectPlayer(selectedPlayer) {
     this.setState({
       player: selectedPlayer
+    })
+  }
+  replay(){
+    this.setState({
+      player : null,
+      winner: null,
+      text : Array(9).fill(null)
     })
   }
 
@@ -34,7 +64,15 @@ class App extends Component {
         <div onClick={() => { this.handleClick(index) }} key={index} className="box">{box}</div>
       )
     })
-    let startPlay = this.state.player ? "" :  <SelectPlayer  player = {(selectedPlayer) =>{this.SelectPlayer(selectedPlayer)}}/>
+   
+  let startPlay = this.state.player ? (
+    <div>
+     <h2>Mr {this.state.player} Play Fast Sir!</h2>
+     <button onClick={() => {this.replay()}}>Replay</button>
+    </div>
+    ) : (
+      <SelectPlayer player={(selectedPlayer) => { this.SelectPlayer(selectedPlayer) }} />
+  );
 
 
     return (
