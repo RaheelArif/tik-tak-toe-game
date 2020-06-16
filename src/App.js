865d3e84b580145ba7bdabe44ddfc40a2535559c
Raheel arif <phuawei292@gmail.com>
2020-06-16 11:41:13 -0700

    ui clear

--- src/App.js
+++ src/App.js
@@ -1,87 +1,21 @@
-import React, { Component } from 'react';
-import PlayerInfo from "./components/PlayerInfo";
-import './App.css';
+import React, { Component } from "react";
+import Score from "./components/score";
+import "./App.css";
+import Nww from "./nww";
 class App extends Component {
   state = {
     text: Array(9).fill(null),
     player: null,
-    winner: null
-  }
-
-  checkWinner() {
-    let winnerStatus = [
-      ["0", "1", "2"],
-      ["3", "4", "5"],
-      ["6", "7", "8"],
-      ["0", "3", "6"],
-      ["1", "4", "7"],
-      ["2", "5", "8"],
-      ["0", "4", "8"],
-      ["2", "4", "6"],
-    ]
-    for (let index = 0; index < winnerStatus.length; index++) {
-      const [a, b, c] = winnerStatus[index]
-      const text = this.state.text 
-      if (text[a] && text[a] === text[b] && text[a] === text[c]) {
-        alert("winner is " + this.state.player)
-
-        this.setState({
-          winner: this.state.player
-        })
-      }
-    }
-  }
-  handleClick = (index) => {
-    if (this.state.player !== null && this.state.winner === null) {
-      let newText = this.state.text
-      if (newText[index] === null) {
-        newText[index] = this.state.player
-        let newPlayer = this.state.player === "X" ? "O" : "X"
-        this.setState({
-          text: newText,
-          player: newPlayer
-        })
-        this.checkWinner()
-      }
-    }
-
-  }
-  SelectPlayer(selectedPlayer) {
-    this.setState({
-      player: selectedPlayer
-    })
-  }
-  replay(){
-    this.setState({
-      player : null,
-      winner: null,
-      text : Array(9).fill(null)
-    })
-  }
+    winner: null,
+  };
 
   render() {
-    const Box = this.state.text.map((box, index) => {
-      return (
-        <div onClick={() => { this.handleClick(index) }} key={index} className="box">{box}</div>
-      )
-    })
-   
-
-
-
     return (
-      <div className="container">
-        <h1>Tik Tak Toe Game</h1>
-        <PlayerInfo player = {this.state.player} SelectPlayer = {(selectedPlayer) =>{this.SelectPlayer(selectedPlayer)}} />
-        <button  disabled={this.state.player === null} onClick={() => {this.replay()}}>Replay</button>
-
-        <div className="box-area">
-          {Box}
-        </div>
-
+      <div>
+<Nww />
+        <Score />
       </div>
-
-    )
+    );
   }
 }
 
---
+++ src/components/score.js
@@ -0,0 +1,243 @@
+import React, { Component } from "react";
+import {
+  exportComponentAsJPEG,
+  exportComponentAsPDF,
+  exportComponentAsPNG,
+} from "react-component-export-image";
+import LinearProgress from "@material/react-linear-progress";
+import Logo from "./images/verdict-logo.svg";
+import earth from "./images/earth.png";
+import lestner from "./images/Brock.jpg";
+import icon1 from "./images/google-play.png";
+import icon2 from "./images/app-store.png";
+import "@material/react-linear-progress/dist/linear-progress.css";
+import "./score.css";
+class Score extends Component {
+  state = {
+    redTotal: 0,
+    redPt: undefined,
+    rounds: [
+      { red: 9.0, blue: 9.2 },
+      { red: 1, blue: 9.8 },
+      { red: 9.83, blue: 1.7 },
+      { red: 10, blue: 9 },
+      { red: 10, blue: 9 },
+    ],
+  };
+  async componentDidMount() {
+    await fetch("https://dayviec25.github.io/test-apis/sample1.json")
+      .then((response) => response.json())
+      .then((data) => this.setState({ data }));
+    console.log(this.state, "aahshs");
+    let red = 0;
+    let blue = 0;
+    for (let i = 0; i < this.state.rounds.length; i++) {
+      red = red + this.state.rounds[i].red;
+      blue = blue + this.state.rounds[i].blue;
+    }
+    let redPT = red / this.state.rounds.length;
+    let bluePT = blue / this.state.rounds.length;
+
+    this.setState({
+      redPt: redPT,
+      bluePt: bluePT,
+      redTotal: red,
+      blueTotal: blue,
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="s-container">
+        <div className="s-box">
+          <div className="s-top">
+            <div className="s-top-l">
+              <h4 className="s-t-l-txt">joanna jedrzejczyk</h4>
+              <img src={lestner} alt="" className="s-t-l-img" />
+            </div>
+            <div className="s-top-c">
+              <img src={Logo} alt="" className="s-t-c-logo" />
+              <div className="t-c-botm">
+                <h4 className="s-t-c-global">Global Scorecard</h4>
+                <img src={earth} alt="" className="s-t-c-gimg" />
+              </div>
+            </div>
+            <div className="s-top-r">
+              <h4 className="s-t-r-txt">Michelle Waterson</h4>
+              <img src={lestner} alt="" className="s-t-r-img" />
+            </div>
+          </div>
+          {this.state.rounds.map((value, index) => {
+            return (
+              <div className="s-mid">
+                <div className="s-m-box">
+                  <h2 className="s-m-h">ROUND {index + 1}</h2>
+                  <div className="sm-new-cc">
+                    <div className="sm-pro">
+                      <div className="sm-pro-lc">
+                        <h4 className="sm-pro-lc-txt">{value.red}</h4>
+
+                        <LinearProgress
+                          className="sm-pro-lc-bx"
+                          progress={value.red / 10}
+                          bufferingDots={false}
+                          buffer={1}
+                          reversed={true}
+                        />
+                      </div>
+                      {value.red - value.blue > 0 ? (
+                        <div className="sm-pro-lc2">
+                          <h4 className="sm-pro-lc-txt2">
+                           1.1
+                          </h4>
+
+                          <LinearProgress
+                            className="sm-pro-lc-bx2"
+                            progress={0.1}
+
+                            bufferingDots={false}
+                            buffer={1}
+                          />
+                        </div>
+                      ) : null}
+                    </div>
+
+                    <div className="sm-diff">
+                      <div className="sm-pro2">
+                        <div className="dif-bb">
+                          <div className="sm-pro-rc">
+                            <LinearProgress
+                              className="sm-pro-rc-bx"
+                              progress={value.blue / 10}
+                              bufferingDots={false}
+                              buffer={1}
+                            />
+                            <h4 className="sm-pro-rc-txt">{value.blue}</h4>
+                          </div>
+                        </div>
+                        {value.blue - value.red > 0 ? (
+                          <div className="sm-pro-rc2">
+                            <LinearProgress
+                              className="sm-pro-rc-bx2"
+                              progress={(value.blue - value.red) / 10}
+                              bufferingDots={false}
+                              buffer={1}
+                            />
+                            <h4 className="sm-pro-rc-txt2">
+                              {(value.blue - value.red).toFixed(2)}+
+                            </h4>
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="h4-dd">Difference</div>
+                </div>
+              </div>
+            );
+          })}
+
+          <div className="s-mid">
+            <div className="s-m-box">
+              <h2 className="s-m-h">FINAL SCORE </h2>
+              <div className="sm-new-cc">
+                <div className="sm-pro2f">
+                  <div className="sm-pro-lc">
+                    <h4 className="sm-pro-lc-txt">+44.76</h4>
+
+                    <LinearProgress
+                      className="sm-pro-lc-bxff"
+                      progress={0.9 }
+                      bufferingDots={false}
+                      buffer={1}
+                      reversed={true}
+
+                    />
+                  </div>
+                  {this.state.redTotal - this.state.blueTotal > 0 ? (
+                    <div className="sm-pro-lc2">
+                      <h4 className="sm-pro-lc-txt22">
+                        +
+                        {(this.state.redTotal - this.state.blueTotal).toFixed(
+                          2
+                        )}
+                      </h4>
+
+                      <LinearProgress
+                        className="sm-pro-lc-bx22f"
+                        progress={4.52 / 10}
+                        bufferingDots={false}
+                        buffer={1}
+                      />
+                    </div>
+                  ) : null}
+                </div>
+
+                <div className="sm-diff">
+                  <div className="sm-pro2f">
+                    <div className="dif-bb">
+                      <div className="sm-pro-rc">
+                        <LinearProgress
+                          className="sm-pro-rc-bx"
+                          progress={45.26 / 5 / 10}
+                          bufferingDots={false}
+                        buffer={1}
+
+                        />
+                        <h4 className="sm-pro-rc-txt">{45.26}</h4>
+                      </div>
+                    </div>
+                    {this.state.blueTotal - this.state.redTotal > 0 ? (
+                      <div className="sm-pro-rc2">
+                        <LinearProgress
+                          className="sm-pro-rc-bx2"
+                          progress={12 / 10}
+                          bufferingDots={false}
+                        buffer={1}
+
+                        />
+                        <h4 className="sm-pro-rc-txt22">
+                          {(this.state.blueTotal - this.state.redTotal).toFixed(
+                            2
+                          )}
+                        </h4>
+                      </div>
+                    ) : null}
+                  </div>
+                </div>
+              </div>
+              <div className="h4-dd">Difference</div>
+            </div>
+          </div>
+          <div className="f-c-footer">
+            <img className="fc-icon1" src={icon1} alt="" />
+            <img className="fc-icon1" src={icon1} alt="" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.componentRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Score ref={this.componentRef} />
+        <button onClick={() => exportComponentAsJPEG(this.componentRef)}>
+          Export As JPEG
+        </button>
+
+        <button onClick={() => exportComponentAsPNG(this.componentRef)}>
+          Export As PNG
+        </button>
+      </React.Fragment>
+    );
+  }
+}
